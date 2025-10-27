b6ef579a5451338e46033ebbb36cf4a4111f0e00
totowang-hhh <tt1224@hotmail.com>
2025-10-27 19:51:01 +0800

    Still working around RAM issues of event module

--- tests/new_event.py
+++ tests/new_event.py
@@ -6,7 +6,8 @@ root = sk.Sk(is_get_context_on_focus=False)
 button = sk.SkTextButton(
     root, text="Print latest event", command=lambda: print(sk.SkEvent.latest.event_type)
 ).box(padx=20, pady=50)
-
-# root.bind("delay[2]", output_latest_event)
+button = sk.SkTextButton(
+    root, text="Print RAM-draining stuff", command=lambda: print("Not implemented")
+).box(padx=20, pady=50)
 
 root.mainloop()
